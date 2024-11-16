03455edb64707d64ccee2f303d6f71ca65436aa0
harshit duggal <135515880+duggal1@users.noreply.github.com>
2024-11-16 12:08:59 +0530

    Update menus.tsx

--- src/constants/menus.tsx
+++ src/constants/menus.tsx
@@ -140,7 +140,7 @@ export const INTEGRATION_LIST_ITEMS: IntegrationsListItemProps[] = [
     id: "1",
     name: "stripe",
     description:
-      "Stripe is the fastest and easiest way to integrate payments and financial services into your software platform or marketplace.",
+      "Stripe is the fastest and easiest way to integrate payments flawlessly and financial services into your software platform or marketplace.",
     logo: "914be637-39bf-47e6-bb81-37b553163945",
     title: "Connect Stripe Account",
     modalDescription:
