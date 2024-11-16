bcd3a1da090da60b60d8b8286b57edb80ed8874e
harshit duggal <135515880+duggal1@users.noreply.github.com>
2024-11-16 11:44:13 +0530

    Update placeholder.tsx

--- src/constants/placeholder.tsx
+++ src/constants/placeholder.tsx
@@ -17,37 +17,37 @@ export type CreateGroupPlaceholderProps = {
 export const CREATE_GROUP_PLACEHOLDER: CreateGroupPlaceholderProps[] = [
   {
     id: "0",
-    label: "Highly engaging",
+    label: "Highly engaging 🚀",
     icon: <MegaPhone />,
   },
   {
     id: "1",
-    label: "Easy to setup",
+    label: "Easy to setup ⚙️",
     icon: <Heart />,
   },
   {
     id: "2",
-    label: "Group chat and posts",
+    label: "Group chat and posts 💬",
     icon: <Chat />,
   },
   {
     id: "3",
-    label: "Students can create teams within Groups",
+    label: "Students can create teams within Groups 👥",
     icon: <Grid />,
   },
   {
     id: "4",
-    label: "Gamification",
+    label: "Gamification 🏆",
     icon: <Document />,
   },
   {
     id: "5",
-    label: "Host unlimited courses",
+    label: "Host unlimited courses 🎓",
     icon: <Courses />,
   },
   {
     id: "6",
-    label: "White-labeling options",
+    label: "White-labeling options 🎨",
     icon: <WhiteLabel />,
   },
 ]
